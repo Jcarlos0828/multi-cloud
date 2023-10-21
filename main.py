8c2b6cec6525d81eb1b0ef4daf49c7a5ea507af1
Jcarlos0828 <jose.carlos4013@gmail.com>
2023-10-21 23:18:35 +0000

    Included CD files and has_space method

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 def is_palindrome(s1: str) -> bool:
+    s1 = ''.join(s1.split(' '))
+    print(s1)
     return s1 == s1[::-1]
-print(is_palindrome('radasr'))
+
+def has_space(s1: str) -> bool:
+    if s1.find(' ') != -1:
+        return False
+    return True
+print(is_palindrome('anita lava la tina'))
--- test_main.py
+++ test_main.py
@@ -1,14 +1,16 @@
-from main import is_palindrome
+from main import is_palindrome, has_space
 
 
 def setup_function(function):
     print(f" Running Setup: {function.__name__}")
     function.x = 'radar'
+    function.y = 'anita lava la tina'
 
 
 def teardown_function(function):
     print(f" Running Teardown: {function.__name__}")
     del function.x
+    del function.y
 
 
 ### Run to see failed test
@@ -17,3 +19,10 @@ def teardown_function(function):
 
 def test_main_isPalindrome():
     assert is_palindrome(test_main_isPalindrome.x) == True
+    assert is_palindrome(test_main_isPalindrome.y) == True
+
+
+def test_main_hasSpace():
+    assert has_space(test_main_hasSpace.x) == True
+    assert has_space(test_main_hasSpace.y) == False
+
